0c388bd7b12f98ed4220166e6563a4777bcbb826
Ayush <k.mishra.ayush98@gmail.com>
2023-12-15 17:06:26 +0530

    Counter Project

---
+++ reactWithVite/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function App() {
+  useState();
+
+  let [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    } else {
+      setCounter(20);
+    }
+  };
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(0);
+    }
+  };
+
+  return (
+    <>
+      <h3>{counter}</h3>
+      <button onClick={addValue}>Increase Value</button>
+      <button onClick={removeValue}>Decrease Value</button>
+    </>
+  );
+}
+
+export default App;
